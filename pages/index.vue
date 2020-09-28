9604f8bd4905670796f52be988b343e87a9f8235
SU A JO <>
2020-09-29 00:07:20 +0900

    index 수정, css 수정

--- pages/index.vue
+++ pages/index.vue
@@ -44,20 +44,14 @@
     <v-flex
       xs12
       border
-      class="section profile bg-color1 mx-height d-flex justify-center align-center"
+      class="section profile bg-color1 mx-height d-flex justify-center align-center  pa-6"
     >
       <div class="content_wrap content_page1">
-        <h2>What is Lorem Ipsum?</h2>
+        <h2>안녕하세요!</h2>
         <div class="d-flex">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          3년차 퍼블리셔 조수아입니다. 시맨틱 마크업, 웹 접근성, 크로스
+          브라우징.. 프론트엔드에 많은 관심을 가지고 개발자가 되기 위해 달려가고
+          있는 중입니다!
         </div>
       </div>
     </v-flex>
@@ -67,10 +61,10 @@
     <v-flex
       xs12
       border
-      class="section skill_wrap bg-color2 mx-height d-flex justify-center align-center"
+      class="section skill_wrap bg-color2 mx-height d-flex justify-center align-center pa-6"
     >
       <div class="content_wrap">
-        <h2>SKILL</h2>
+        <h2>skill</h2>
         <div class="d-flex justify-end">
           <div>ff</div>
           <div class="d-flex justify-end">
@@ -85,99 +79,72 @@
     <v-flex
       xs12
       border
-      class="section portfolio_wrap bg-color3 mx-height d-flex justify-center align-center"
+      class="section portfolio_wrap bg-color3 mx-height d-flex justify-center align-center pa-6"
     >
       <div class="content_wrap">
-        <h2>HEAD</h2>
-        <div class="d-flex">
-          <v-carousel
-            cycle
-            height="auto"
-            hide-delimiter-background
-            show-arrows-on-hover
+        <h2>portfolio</h2>
+        <div class="d-flex flex-wrap">
+          <v-card
+            class="mx-auto col-sm-5 col-xs-12"
+            color="grey lighten-4"
+            max-width="600"
+            v-for="(item, i) in items"
+            :key="i"
           >
-            <v-carousel-item v-for="(slide, i) in slides" :key="i">
-              <v-sheet :color="colors[i]" height="100%">
-                <v-row class="fill-height" align="center" justify="center">
-                  <v-hover v-slot:default="{ hover }">
-                    <v-card
-                      class="mx-auto"
-                      color="grey lighten-4"
-                      max-width="600"
-                    >
-                      <v-img
-                        :aspect-ratio="16 / 9"
-                        src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
-                      >
-                        <v-expand-transition>
-                          <div
-                            v-if="hover"
-                            class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
-                            style="height: 100%;"
-                          >
-                            동천 10th
-                          </div>
-                        </v-expand-transition>
-                      </v-img>
-                      <v-card-text class="pt-6" style="position: relative;">
-                        <v-btn
-                          absolute
-                          color="orange"
-                          class="white--text"
-                          fab
-                          large
-                          right
-                          top
-                          href="https://4004103.github.io/d10th/"
-                        >
-                          <v-icon>reply</v-icon>
-                        </v-btn>
-                        <div class="font-weight-light grey--text title mb-2">
-                          For the perfect meal
-                        </div>
-                        <h3
-                          class="display-1 font-weight-light orange--text mb-2"
-                        >
-                          QW cooking utensils
-                        </h3>
-                        <div class="font-weight-light title mb-2">
-                          Our Vintage kitchen utensils delight any chef.<br />
-                          Made of bamboo by hand
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </v-hover>
-                </v-row>
-              </v-sheet>
-            </v-carousel-item>
-          </v-carousel>
+            <v-img :aspect-ratio="16 / 9" :src="item.src"> </v-img>
+            <v-card-text class="pt-6" style="position: relative;">
+              <v-btn
+                absolute
+                color="orange"
+                class="white--text"
+                fab
+                large
+                right
+                top
+                :href="item.href"
+              >
+                <v-icon>reply</v-icon>
+              </v-btn>
+              <div
+                class="font-weight-light grey--text title mb-2"
+                v-text="item.grayTitle"
+              ></div>
+              <h3
+                class="display-1 font-weight-light orange--text mb-2"
+                v-text="item.orangeTitle"
+              ></h3>
+              <div
+                class="font-weight-light title mb-2"
+                v-text="item.text"
+              ></div>
+              <div class="demo_btn">
+                <v-btn
+                  class="ma-2"
+                  outlined
+                  color="red darken-3"
+                  :href="item.mainDemo"
+                  >Main Demo</v-btn
+                >
+                <v-btn
+                  class="ma-2"
+                  outlined
+                  color="blue darken-4"
+                  :href="item.subDemo"
+                  >sub Demo</v-btn
+                >
+              </div>
+            </v-card-text>
+          </v-card>
         </div>
       </div>
     </v-flex>
     <!-- e: what -->
 
-    <v-flex xs12 class="section portfolio d-flex justify-center align-center">
-      <v-carousel
-        cycle
-        height="400"
-        hide-delimiter-background
-        show-arrows-on-hover
-      >
-        <v-carousel-item v-for="(slide, i) in slides" :key="i">
-          <v-sheet :color="colors[i]" height="100%">
-            <v-row class="fill-height" align="center" justify="center">
-              <div class="display-3">{{ slide }} Slide</div>
-            </v-row>
-          </v-sheet>
-        </v-carousel-item>
-      </v-carousel>
-    </v-flex>
     <v-flex>
       <v-bottom-navigation v-model="bottomNav">
         <v-btn value="recent">
           <span>Recent</span>
           <v-icon>history</v-icon>
-          <v-slot>djfksjdfljsdfkdjfkds</v-slot>
         </v-btn>
 
         <v-btn value="favorites">
@@ -204,20 +171,43 @@ export default {
       "brown lighten-2",
       "brown lighten-1",
     ],
-    slides: ["First", "Second", "Third", "Fourth", "Fifth"],
     bottomNav: "recent",
     items: [
       {
-        color: "#1F7087",
-        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-        title: "Supermodel",
-        artist: "Foster the People",
+        src: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
+        href: "https://4004103.github.io/d10th/",
+        grayTitle: "Foster the People",
+        orangeTitle: "동천 10th",
+        text: "ajkdfadjfkajdlfjadkfd",
+        mainDemo: "https://4004103.github.io/d10th/",
+        subDemo: "https://4004103.github.io/d10th/index_today.html",
       },
       {
-        color: "#952175",
-        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-        title: "Halcyon Days",
-        artist: "Ellie Goulding",
+        src: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
+        href: "https://4004103.github.io/jive/",
+        grayTitle: "ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ",
+        orangeTitle: "Jive Solutions",
+        text: "ㅉㅉㅉㅉㅉㅉㅈ",
+        mainDemo: "https://4004103.github.io/jive/",
+        subDemo: "https://4004103.github.io/jive/sub01.html",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
+        href: "https://4004103.github.io/jive/",
+        grayTitle: "3",
+        orangeTitle: "ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ",
+        text: "ㅉㅉㅉㅉㅉㅉㅈ",
+        mainDemo: "",
+        subDemo: "",
+      },
+      {
+        src: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
+        href: "https://4004103.github.io/jive/",
+        grayTitle: "4",
+        orangeTitle: "ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ",
+        text: "ㅉㅉㅉㅉㅉㅉㅈ",
+        mainDemo: "",
+        subDemo: "",
       },
     ],
   }),
